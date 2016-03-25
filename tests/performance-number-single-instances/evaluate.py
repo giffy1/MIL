b1708dd465ebc227bff7e3ae6359d357e9d81abb
giffy1 <giffy1991@gmail.com>
2016-03-25 15:44:18 -0400

    Added n_cores option when calling qsub for server efficiency

--- tests/performance-number-single-instances/evaluate.py
+++ tests/performance-number-single-instances/evaluate.py
@@ -7,11 +7,13 @@ participants = range(20)
 #N = 5
 M = range(100,1001,50)
 
-def qsub(command, job_name=None, stdout=None, stderr=None, depend=None):
+def qsub(command, job_name=None, stdout=None, stderr=None, depend=None, n_cores=None):
 	"""
 	depend could be either a string or a list (or tuple, etc.)
 	"""
 	args = ['qsub']
+	if n_cores:
+		args.extend(['-pe','generic',"%d"%n_cores])
 	if job_name:
 		args.extend(['-N', job_name])
 	if stderr:
