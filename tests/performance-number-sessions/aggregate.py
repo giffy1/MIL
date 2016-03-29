decd2cfaa7946e47073d243a1a5a7e7622a01531
giffy1 <giffy1991@gmail.com>
2016-03-29 08:30:17 -0400

    Plot results by number of sessions

--- tests/performance-number-sessions/aggregate.py
+++ tests/performance-number-sessions/aggregate.py
@@ -17,47 +17,35 @@ matplotlib.use('Agg') #ensures plot can be viewed on server
 from matplotlib import pyplot
 
 participants = range(20)
-bag_sizes = [100,200,300]
-N = {}
-N[30] = range(0,81,5)
-N[60] = range(0,41,4)
-N[90] = range(0,37,3)
-N[120] = range(0,25,3)
-N[150] = range(0,19,2)
-N[180] = range(0,15,2)
-N[210] = range(0,13,2)
-N[240] = range(0,9,1)
-N[270] = range(0,7,1)
-N[300] = range(0,7,1)
+# number of positive sessions for each participant:
+N = [4, 3, 4, 4, 4, 3, 3, 3, 4, 4, 3, 4, 3, 4, 3, 3, 4, 4, 4, 3]
 
 def main(working_dir, save_path):
 	
 	res_dir = working_dir + '/res'
 	
 	fscores = []
-	for b in bag_sizes:
-		fscores = []
-		for n in N[b]:
-			avg_fscore = 0
-			count = 0
-			for p in participants:
-				res_path = os.path.join(res_dir, 'lopo_p%d_n%d_b%d.pickle' % (p, n, b))
-				if os.path.isfile(res_path):
-					with open(res_path, 'rb') as f:
-						r = pickle.load(f)
-					avg_fscore += r['Results']['F1 Score']['Test']
-					count += 1
-			if count == 0:
-				fscores.append(0)
-			else:
-				fscores.append(avg_fscore / count)
+	for n in range(4):
+		avg_fscore = 0
+		count = 0
+		for p in participants:
+			res_path = os.path.join(res_dir, 'lopo_p%d_n%d.pickle' % (p, n))
+			if os.path.isfile(res_path):
+				with open(res_path, 'rb') as f:
+					r = pickle.load(f)
+				avg_fscore += r['Results']['F1 Score']['Test']
+				count += 1
+		if count == 0:
+			fscores.append(0)
+		else:
+			fscores.append(avg_fscore / count)
 		
-		pyplot.figure()
-		pyplot.plot(N[b], fscores)
-		pyplot.title("sbMIL performance varying number of bags; bag-size = " + str(b))
-		pyplot.xlabel("number of bags N")
-		pyplot.ylabel("F1 Score")
-		pyplot.savefig(save_path + '_b' + str(b) + '.png')
+	pyplot.figure()
+	pyplot.plot(range(4), fscores)
+	pyplot.title("sbMIL performance varying number of positive sessions")
+	pyplot.xlabel("number of bags N")
+	pyplot.ylabel("F1 Score")
+	pyplot.savefig(save_path + 'sessions.png')
 	
 
 if __name__ == '__main__':
