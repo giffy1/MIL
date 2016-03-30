b561ceebf5bcb00cf191e6afae271fe1526eef02
giffy1 <giffy1991@gmail.com>
2016-03-29 21:50:32 -0400

    Updated aggregate to be compatible with evaluate.py results

--- tests/performance-loss-by-bag-size/aggregate.py
+++ tests/performance-loss-by-bag-size/aggregate.py
@@ -17,36 +17,37 @@ matplotlib.use('Agg') #ensures plot can be viewed on server
 from matplotlib import pyplot
 
 participants = range(20)
-#bag_size = 200
-#N = 5
-M = range(100,1001,50)
+M = range(0,501,50)
+bag_size = [1,60,120,180,240,300]
 
 def main(working_dir, save_path):
 	
 	res_dir = working_dir + '/res'
 	
-	fscores = []
+
 	for m in M:
-		avg_fscore = 0
-		count = 0
-		for p in participants:
-			res_path = os.path.join(res_dir, 'lopo_p%d_m%d.pickle' % (p, m))
-			if os.path.isfile(res_path):
-				with open(res_path, 'rb') as f:
-					r = pickle.load(f)
-				avg_fscore += r['Results']['F1 Score']['Test']
-				count += 1
-		if count == 0:
-			fscores.append(0)
-		else:
-			fscores.append(avg_fscore / count)
+		fscores = []
+		for b in bag_size:
+			avg_fscore = 0
+			count = 0
+			for p in participants:
+				res_path = os.path.join(res_dir, 'lopo_p%d_m%d_b%d.pickle' % (p, m, b))
+				if os.path.isfile(res_path):
+					with open(res_path, 'rb') as f:
+						r = pickle.load(f)
+					avg_fscore += r['Results']['F1 Score']['Test']
+					count += 1
+			if count == 0:
+				fscores.append(0)
+			else:
+				fscores.append(avg_fscore / count)
 		
-	pyplot.figure()
-	pyplot.plot(M, fscores)
-	pyplot.title("sbMIL performance varying number of training instances")
-	pyplot.xlabel("number of bags N")
-	pyplot.ylabel("F1 Score")
-	pyplot.savefig(save_path + '.png')
+		pyplot.figure()
+		pyplot.plot(M, fscores)
+		pyplot.title("sbMIL performance varying bag size: M = " + str(M))
+		pyplot.xlabel("Bag Size")
+		pyplot.ylabel("F1 Score")
+		pyplot.savefig(save_path + '_M' + str(M) + '.png')
 	
 
 if __name__ == '__main__':
