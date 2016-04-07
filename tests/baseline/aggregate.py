aef7a097ef33f22a435e19bfc8a18d13a8ade23d
giffy1 <giffy1991@gmail.com>
2016-04-07 15:35:21 -0400

    Compute average f score using total confusion matrix

--- tests/baseline/aggregate.py
+++ tests/baseline/aggregate.py
@@ -11,6 +11,11 @@ import os
 import pickle
 import matplotlib
 from argparse import ArgumentParser
+import numpy as np
+import sys
+
+sys.path.insert(0, '../../src/')
+from util import accuracy_precision_recall_fscore
 
 matplotlib.use('Agg') #ensures plot can be viewed on server
 
@@ -22,17 +27,20 @@ def main(working_dir):
 	
 	avg_fscore = 0
 	count = 0
+	total_conf = np.zeros((2,2))
 	for p in participants:
 		res_path = os.path.join(res_dir, 'lopo_p%d.pickle' %p)
 		if os.path.isfile(res_path):
 			with open(res_path, 'rb') as f:
 				r = pickle.load(f)
 			conf = r['Results']['Confusion Matrix']['Test']
-			fscore = r['Results']['F1 Score']['Test']
-			print conf
-			#print("F1 Score for participant %d : %0.2f%%" %(p, 100*fscore))
-			avg_fscore += fscore
+			total_conf += conf
 			count += 1
+	total_conf /= count
+	print("Total Confusion Matrix ")
+	print(total_conf)
+	_, prf = accuracy_precision_recall_fscore(total_conf)
+	print prf
 	print("Average F1 Score : %0.2f%% " %(100*avg_fscore / count))
 	
 
