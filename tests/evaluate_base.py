5dde788a6334a72cd6f5ec93040afb26b05ae5b7
giffy1 <giffy1991@gmail.com>
2016-04-11 13:29:48 -0400

    Quick experiment with random number of jobs per process

--- tests/evaluate_base.py
+++ tests/evaluate_base.py
@@ -10,6 +10,7 @@ import os
 import sys
 from qsub import qsub
 import itertools
+import numpy as np
 		
 class BaseEvaluator():
 	
@@ -139,7 +140,7 @@ class BaseEvaluator():
 						file_str = '_p%d' %p + self._tuple_to_str(self.variables.keys(), vals)
 					
 					save_path = os.path.join(self.res_dir, 'lopo' + file_str + '.pickle')
-					submit_this_job = 'python %s/w_lopo.py --save=%s --test-participant=%d --cv=%d' % (self.args.src, save_path, p, n_iter) + self.arg_str + var_arg_str
+					submit_this_job = 'python %s/w_lopo.py --save=%s --test-participant=%d --cv=%d --n-jobs=%d' % (self.args.src, save_path, p, n_iter, np.random.randint(1,7)) + self.arg_str + var_arg_str
 					print submit_this_job + '\n'
 					job_id = 'lopo' + file_str
 					log_file = os.path.join(self.log_dir, 'log' + file_str + '.txt')
@@ -154,6 +155,6 @@ class BaseEvaluator():
 		
 		self.arg_str = ''
 		for arg in self.args._get_kwargs():
-			if arg[0] not in {'src', 'dir', 'cv', 'n_jobs_per_iter'} and arg[0] not in self.variables:
+			if arg[0] not in {'src', 'dir', 'cv', 'n_jobs_per_iter', 'n_jobs'} and arg[0] not in self.variables:
 				self.arg_str += ' --' + arg[0].replace('_', '-') + '="' + str(arg[1]) + '"'
 		self._save_params()
