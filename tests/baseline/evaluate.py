cdb836715830250b231d2511c6109646ccbcbda2
giffy1 <giffy1991@gmail.com>
2016-04-11 12:24:35 -0400

    run basic evaluation with no varied parameters

--- tests/baseline/evaluate.py
+++ tests/baseline/evaluate.py
@@ -5,7 +5,7 @@ from evaluate_base import BaseEvaluator
 
 def main():	
 
-	base = BaseEvaluator({'N' : range(10,101,10), 'M' : range(100,1001,100), 'bag_size' : [10,25,50,100,200]})
+	base = BaseEvaluator({})
 	base.evaluate()
 
 if __name__ == '__main__':
