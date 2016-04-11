c6874cb04141ab5bdfa112c49185dec8cb3d10cf
giffy1 <giffy1991@gmail.com>
2016-04-11 08:20:45 -0400

    Added n_jobs_per_iter to list of params to ignore when propogating params to w_lopo because that param does not exist in w_lopo

--- tests/baseline/evaluate.py
+++ tests/baseline/evaluate.py
@@ -76,7 +76,7 @@ def main():
 	
 	arg_str = ''
 	for arg in args._get_kwargs():
-		if arg[0] != 'src' and arg[0] != 'dir' and arg[0] != 'cv':
+		if arg[0] not in {'src', 'dir', 'cv', 'n_jobs_per_iter'}:
 			arg_str += ' --' + arg[0].replace('_', '-') + '="' + str(arg[1]) + '"'
 		
 	with open(os.path.join(params_dir, 'params.txt'), 'wb') as f:
