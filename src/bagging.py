306ecb35e71e1ef6434df93b9b343a33b80ad83d
giffy1 <giffy1991@gmail.com>
2016-04-14 10:08:08 -0400

    New files with significant changes to lopo evaluation, primarily in data split used for cv

---
+++ src/bagging.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 12 13:50:49 2016
+
+@author: snoran
+"""
+
+import numpy as np
+from util import shuffle
+import sys
+import pickle
+from argparse import ArgumentParser
+
+MIL = {'SIL', 'sMIL', 'sbMIL', 'misvm', 'MIForest'}
+
+def single_instances_to_sessions(X, Y, session_labels, session_start):
+	"""
+	TODO: Doc String
+	"""
+	bags = []
+	labels = []
+	single_instance_labels = []
+	for j in range(len(session_labels)):
+		if j < len(session_labels)-1:
+			end = session_start[j+1]
+		else:
+			end = len(X)
+		bags.append(X[session_start[j]:end, :])
+		single_instance_labels.append(Y[session_start[j]:end])
+		labels.append(session_labels[j])
+			
+	return bags, labels, single_instance_labels
+	
+def main(data_dir, data_file, bag_size, active_participant_counter):
+
+#data_dir = '../data/eating_detection_inertial_ubicomp2015/'
+#data_dir = '../data/smoking-data/'
+#data_file = "data_p0.pickle"
+
+	sys.path.insert(0, data_dir)
+	from load_data import load_data
+	
+	dataset = load_data(data_dir)
+	X = dataset['data']['X']
+	Y = dataset['data']['Y']
+	session_start = dataset['data']['sessions']['start']
+	session_labels = dataset['data']['sessions']['labels']	
+	print data_dir
+	print dataset['description']
+	
+	participant_indices = range(len(X))
+	n_si_participants = 5
+	n_bag_participants = len(X) - n_si_participants - 1
+	
+	#indices for participants in training data; skip active participant counter:
+	train_indices = participant_indices[:active_participant_counter] + participant_indices[active_participant_counter+1:]	
+	
+	si_participant_indices = train_indices[:n_si_participants]
+	bag_participant_indices = train_indices[n_si_participants:n_si_participants+n_bag_participants+1]
+	
+	#single-instance training data:
+	X_SI = []
+	Y_SI = []
+	for p in si_participant_indices:
+		x = X[p]
+		y = Y[p]
+		x,y = shuffle(x,y)
+		X_SI.append(x)
+		Y_SI.append(y)
+	
+	#bag-level training data:
+	X_B = []
+	Y_B = []
+	for p in bag_participant_indices:
+		if bag_size == -1:
+			x, y, _ = single_instances_to_sessions(X[p], Y[p], session_labels[p], session_start[p])
+		else:
+			x = [X[p][k:k+bag_size, :] for k in xrange(0, len(X[p]), bag_size)]
+			y = [max(Y[p][k:k+bag_size]) for k in xrange(0, len(Y[p]), bag_size)]
+		x,y = shuffle(x,y)	
+		X_B.append(x)
+		Y_B.append(y)
+		
+	#training data from the held-out participant:
+	#TODO: ^
+	
+	#test data:
+	X_test = X[active_participant_counter]
+	Y_test = Y[active_participant_counter]
+	X_test, Y_test = shuffle(X_test, Y_test)
+
+##convert to bags:
+#if clf_name in MIL:
+#	X_SI = [X_SI[k:k+1, :] for k in xrange(len(X_SI))]
+#	Y_SI = [max(Y_SI[k:k+1]) for k in xrange(len(Y_SI))]
+#	
+#	if bag_size == -1:
+#		X_B, Y_B, _ = single_instances_to_sessions(X, Y, session_labels, session_start, bag_participant_indices)
+#	else:
+#		X_B = [X_B[k:k+bag_size, :] for k in xrange(0, len(X_B), bag_size)]
+#		Y_B = [max(Y_B[k:k+bag_size]) for k in xrange(0, len(Y_B), bag_size)]
+#	
+#	if held_out_bag_size == -1:
+#		X_T, Y_T, Y_si = single_instances_to_sessions(X, Y, session_labels, session_start, [active_participant_counter])
+#	else:
+#		X_T = [X_test[k:k+bag_size, :] for k in xrange(0,len(X_test), held_out_bag_size)]
+#		Y_si = [Y_test[k:k+bag_size] for k in xrange(0,len(Y_test), held_out_bag_size)]
+#		Y_T = [max(y_t) for y_t in Y_si]
+#						
+#	X_T, Y_T, Y_si = shuffle(X_T, Y_T, Y_si)	
+#
+#	# convert remaining bags back to test instances
+#	X_test = []
+#	Y_test = []
+#	for i, (x_t, y_si) in enumerate(zip(X_T, Y_si)[K:]):
+#		for (x,y) in zip(x_t, y_si):
+#			X_test.append(x)
+#			Y_test.append(y)
+#			
+#	X_test = [np.asarray(X_test)[k:k+test_bag_size, :] for k in xrange(0, len(X_test), test_bag_size)]
+#	Y_test = [max(Y_test[k:k+test_bag_size]) for k in xrange(0, len(Y_test), test_bag_size)]
+#
+#else: # standard supervised learning case
+#	X_T = X_test[:K]
+#	X_T = X_test[:K]
+#	X_test = X_test[K:]
+#	Y_test = Y_test[K:]
+#	
+#if N < 0:
+#	N=len(X_B)
+#	
+#if M < 0:
+#	M=len(X_SI)
+#		
+#X_SI, Y_SI = shuffle(X_SI, Y_SI)
+#X_B, Y_B = shuffle(X_B, Y_B)
+#X_test, Y_test = shuffle(X_test, Y_test)
+
+	data = {}
+	data['training'] = {'instance' : {'X' : X_SI, 'Y' : Y_SI}, 'bag' : {'X' : X_B, 'Y' : Y_B}}
+	data['test'] = {'X' : X_test, 'Y' : Y_test}
+	
+	with open(data_file, 'wb') as f:
+		pickle.dump(data, f)
+
+# print number of bags per participant	
+# [len(X_B[k]) for k in range(len(X_B))]
+
+# print size of bags for a particular subject
+# [len(X_B[p][k]) for k in range(len(X_B[p]))]
+	
+# print number of instances per participant	
+# [len(X_SI[k]) for k in range(len(X_SI))]
+	
+# TODO: If we set M=0, then we can't really do CV, right? One way around this 
+# is to use a positive M but never put them in the training data
+
+if __name__ == "__main__":
+	main('../data/eating_detection_inertial_ubicomp2015/', 'data_p0.pickle', 10, 0)
+
+	parser = ArgumentParser()
+	
+	parser.add_argument("-d", "--data-dir", dest="data_dir", default='../data/eating_detection_inertial_ubicomp2015/', type=str, \
+			help="Directory where the dataset is stored.")
+	parser.add_argument("-s", "--save", dest="data_file", default='data.pickle', type=str, \
+			help="File where the bagged data will be stored.")
+	parser.add_argument("-p", "--participant", dest="active_participant_counter", default=0, type=int, \
+			help="Participant held out for evaluating the model.")	
+	parser.add_argument("-b", "--bag-size", dest="bag_size", default=-1, type=int, \
+			help="Bag Size (-1 for sessions)")
+	
+	args = parser.parse_args()
+	
+	main(**vars(args))
---
+++ src/lopo.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 17:22:34 2016
+
+@author: snoran
+
+Leave-one-participant-out evaluation over Lab-20 eating dataset. Evaluation 
+can be done using standard supervised learning methods or multi-instance learning. 
+If multi-instance learning is used, then the classifier may be trained using 
+either or both single-instance bags or larger bags. Additionally, training data 
+may include labelled instances or bags from the held-out participant, in order 
+to enhance generalizability.
+"""
+
+import numpy as np
+from sklearn.metrics import confusion_matrix, classification_report
+from time import time
+from sklearn.grid_search import GridSearchCV, RandomizedSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC, LinearSVC
+import misvm
+import pickle
+from argparse import ArgumentParser
+from util import farey, accuracy_precision_recall_fscore, pprint_header, score, mil_train_test_split
+from miforest.miforest import MIForest
+import json
+
+MIL = {'SIL', 'sMIL', 'sbMIL', 'misvm', 'MIForest'}
+
+def get_param_grid_by_clf(clf_name, kernel='linear'):
+	"""
+	Generates a reasonable parameter grid (i.e. for cross-validation) for the 
+	given classifier. For SVM based techniques (sbMIL, SIL, SVC), this includes 
+	generating a list of C values and class_weight pairs. 
+	"""	
+	
+	#class weights are determined by a Farey sequence to make sure that redundant pairs, 
+	#i.e. (1,1) = (2,2), (2,3) = (4,6), etc. are not included.
+	class_weights = [{1 : i, -1 : j} for (i,j) in farey(25)[1:]] #ignore first value where i=0
+	class_weights.extend([{1 : j, -1 : i} for (i,j) in farey(25)[1:]]) #swap i and j, ignore first value
+	C_array = np.logspace(-5, 15, 21, base=2).tolist()
+	gamma_array = np.logspace(-15, 3, 19, base=2).tolist()
+	eta_array = np.linspace(0,1,9).tolist()
+	n_estimators_array = [25,50,75,100,125,150]
+	param_grid = {}
+	
+	if clf_name in {'RF', 'MIForest'}:
+		param_grid.update({'n_estimators' : n_estimators_array})
+	
+	if clf_name in {'SIL', 'sMIL', 'sbMIL', 'RF', 'SVM', 'LinearSVC'}:
+		param_grid.update({'class_weight' : class_weights})
+		
+	if clf_name in {'SIL', 'sMIL', 'sbMIL', 'misvm', 'SVM', 'LinearSIL', 'LinearSVC'}:
+		param_grid.update({'C' : C_array})
+	
+	if clf_name in {'SIL', 'sMIL', 'sbMIL', 'misvm', 'SVM'} and kernel == 'rbf':
+		param_grid.update({'gamma' : gamma_array})
+		
+	if clf_name == 'sbMIL':
+		param_grid.update({'eta' : eta_array})
+		
+	return param_grid
+	
+def get_clf_by_name(clf_name, **kwargs):
+	"""
+	Returns an instance of a classifier given its name and its parameters. 
+	The parameters are specified as key-word arguments to the method, following 
+	the name of the classifier. Any parameter may be included as long as it 
+	is included in the classifier implementation.
+	
+	@param clf_name The name of the classifier
+	@param kwargs key-word arguments including the model parameters
+	"""
+	
+	if clf_name == 'RF':
+		clf = RandomForestClassifier(**kwargs)
+	elif clf_name == 'SVM':
+		clf = SVC(**kwargs)
+	elif clf_name == 'SIL':
+		clf = misvm.SIL(**kwargs)
+	elif clf_name == 'MIForest':
+		clf = MIForest(**kwargs)
+	elif clf_name == 'sMIL':
+		clf = misvm.sMIL(**kwargs)
+	elif clf_name == 'sbMIL':
+		clf = misvm.sbMIL(**kwargs)
+	elif clf_name == 'misvm':
+		clf = misvm.MISVM(**kwargs)
+	elif clf_name == 'LinearSIL':
+		clf = misvm.LinearSIL(**kwargs)
+	elif clf_name == 'LinearSVC':
+		clf = LinearSVC(**kwargs)
+	return clf
+	
+def parse_clf(clf_str):
+	start = clf_str.index('(')
+	end = clf_str.index(')')
+	clf_name = clf_str[:start]
+	param_list_str = clf_str[start+1:end]
+	param_lst = param_list_str.split(',')
+	params = {}
+	for param_str in param_lst:
+		if '=' in param_str:
+			split_index = param_str.index('=')
+			s = param_str[split_index+1:].strip()
+			params[param_str[:split_index].strip()] = json.loads(s)
+	return clf_name, params
+
+def main(data_file, clf_str, cv_method, kfolds, n_iter, n_jobs, n_trials, verbose, save, description):
+	"""
+	TODO: Doc string
+	"""
+	
+	with open(data_file, 'rb') as f:
+		data = pickle.load(f)
+		
+	X_SI = data['training']['instance']['X']
+	Y_SI = data['training']['instance']['Y']
+	X_B = data['training']['bag']['X']
+	Y_B = data['training']['bag']['Y']
+	X_train = []
+	Y_train = []
+
+	for p in range(len(X_SI)):
+		X_train.extend(X_SI[p])
+		Y_train.extend(Y_SI[p])
+	for p in range(len(X_B)):
+		X_train.extend(X_B[p])
+		Y_train.extend(Y_B[p])
+#	X_train = list(np.vstack(X_SI)) + list(np.hstack(X_B))
+#	Y_train = list(np.hstack(Y_SI)) + list(np.hstack(Y_B))
+	X_test = data['test']['X']
+	Y_test = data['test']['Y']
+	
+	clf_name, clf_params = parse_clf(clf_str)
+	clf = get_clf_by_name(clf_name, **clf_params)
+	param_grid = get_param_grid_by_clf(clf_name, clf_params.get("kernel", "linear"))
+
+	results = {
+		"Confusion Matrix" : {"Training" : np.zeros((2,2)), "Test" : np.zeros((2,2))}, \
+		"Precision": {"Training" : 0.0, "Test" : 0.0}, \
+		"Recall": {"Training" : 0.0, "Test" : 0.0}, \
+		"F1 Score": {"Training" : 0.0, "Test" : 0.0, "Validation" : 0.0} \
+	}
+
+	cv_iterator = mil_train_test_split(X_SI, X_B, 15, 20)
+
+	if cv_method == 'grid':
+		gs = GridSearchCV(clf, param_grid, scoring=score, cv=cv_iterator, verbose=verbose, n_jobs = n_jobs)
+	elif cv_method == 'randomized':
+		gs = RandomizedSearchCV(clf, param_distributions=param_grid, scoring=score, cv=cv_iterator, n_jobs = n_jobs, n_iter=n_iter, verbose=verbose)
+	
+	t0 = time()
+	gs = gs.fit(X_train, Y_train)
+	tf = time()
+	
+	print("Best parameters set found on development set:\n")
+	print(gs.best_params_)
+	print("\nGrid scores on development set:\n")
+	for params, mean_score, scores in gs.grid_scores_:
+		print("%0.3f (+/-%0.03f) for %r"
+		% (mean_score, scores.std() * 2, params))
+	
+	print("\nDetailed classification report:\n")
+	print("The model is trained on the full development set.")
+	print("The scores are computed on the full evaluation set.\n")
+	y_true, y_pred = Y_test, 2*np.greater(gs.predict(X_test),0)-1
+	print(classification_report(y_true, y_pred))
+
+	print("\nTime elapsed: %0.2f seconds." %(tf-t0))
+	
+#	if clf_name == 'MIForest': #for MIForest, we need to pass in Y as well
+#		#check training accuracy to start:
+#		y_pred = 2*np.greater(gs.best_estimator_.predict(X_train, Y_train),0)-1	
+#	else: #for MIForest, we need to pass in Y as well
+#		#check training accuracy to start:
+#		y_pred = 2*np.greater(gs.best_estimator_.predict(X_train),0)-1
+#	
+#	conf = confusion_matrix(Y_train, y_pred, [-1,+1])
+#	print("Confusion matrix on the training data:")
+#	print(conf)
+#	results['Confusion Matrix']['Training'] = conf
+#	
+#	precision, recall, fscore = accuracy_precision_recall_fscore(conf)[1][1]
+#	results['F1 Score']['Training'] = fscore
+#	results['Precision']['Training'] = precision
+#	results['Recall']['Training'] = recall	
+	
+	
+#	if clf_name == 'MIForest':
+#		y_pred = 2*np.greater(gs.best_estimator_.predict(X_test, Y_test),0)-1
+#	else:
+#		y_pred = 2*np.greater(gs.best_estimator_.predict(X_test),0)-1
+#		
+	conf = confusion_matrix(y_true, y_pred, [-1,+1])
+	print("Confusion matrix on the test data:")
+	print(conf)
+	results['Confusion Matrix']['Test'] = conf
+		
+	precision, recall, fscore = accuracy_precision_recall_fscore(conf)[1][1]
+	results['F1 Score']['Test'] = fscore
+	results['Precision']['Test'] = precision
+	results['Recall']['Test'] = recall	
+	
+	print("Precision on the test data: %0.2f%%" %(100*precision))
+	print("Recall on the test data: %0.2f%%" %(100*recall))
+	print("F1 Score on the test data: %0.2f%%\n" %(100*fscore))
+	
+	evaluation = {"Description": description, "Results" : results}	
+	if save != 'none':
+		print("Saving results to %s ..." %save)
+
+		with open(save, 'wb') as f:
+			pickle.dump(evaluation, f)
+		
+	return evaluation
+		
+if __name__ == "__main__":
+	parser = ArgumentParser()
+	
+	parser.add_argument("-d", "--data", dest="data_file", default='data_p0.pickle', type=str, \
+			help="Directory where the dataset is stored")
+
+	parser.add_argument("--clf", dest="clf_str", default='sbMIL(verbose=0)', type=str, \
+			help="Classifier ('RF', 'SVM', 'LinearSVC', 'SIL', 'LinearSIL', 'MIForest', 'sMIL', 'sbMIL', 'misvm')")
+			
+	parser.add_argument("--cv-method", dest="cv_method", default='randomized', type=str, \
+			help="Determines how hyperparameters are learned ('grid' or 'randomized')")
+	parser.add_argument("--cv", dest="kfolds", default=5, type=int, \
+			help="Determines split for cross-validation (see GridSearchCV.cv)")
+	parser.add_argument("--n-iter", dest="n_iter", default=10, type=int, \
+			help="The number of iterations in randomized cross-validation (see RandomizedSearchCV.cv)")
+	parser.add_argument("--n-trials", dest="n_trials", default=5, type=int, \
+			help="Number of trials over which to average the performance metrics")
+	parser.add_argument("--n-jobs", dest="n_jobs", default=1, type=int, \
+			help="Number of threads used (default = 1). Use -1 for maximal parallelization")
+			
+	parser.add_argument("--verbose", dest="verbose", default=0, type=int, \
+			help="Indicates how much information should be reported (0=None, 1=Some, 2=Quite a bit)")
+	parser.add_argument("--save", dest="save", default='none', type=str, \
+			help="Path of the pickle file containing the data. If none (default), the data will not be pickled")	
+	parser.add_argument("--desc", dest="description", default='MIForest test.', type=str, \
+			help="Description of the evaluation and parameter selection")
+	
+	args = parser.parse_args()
+	
+	results = main(**vars(args))
