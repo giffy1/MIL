e3ba99551a5124bacf599167754abf727c4893ac
giffy1 <giffy1991@gmail.com>
2016-04-15 10:21:52 -0400

    Realized that best params were not stored in pickle files but are displayed in the log fils

--- tests/param_grid_check.py
+++ tests/param_grid_check.py
@@ -6,16 +6,14 @@ Created on Fri Apr 15 10:02:08 2016
 """
 
 import os
-import pickle
 for root, dirs, files in os.walk("."):
 	for f in files:
-		if f.endswith(".pickle") and f.startswith("lopo"):
-			for s in f[f.index("_"):f.index(".pickle")].split("_"):
+		if f.endswith(".txt") and f.startswith("log"):
+			for s in f[f.index("_"):f.index(".txt")].split("_"):
 				if len(s) > 0:
 					if s[0] == "p":
 						participant = int(s[1:])
 			with open(os.path.join(root, f), 'rb') as fd:
-				r = pickle.load(fd)
-			k = r["Results"].keys()
-			print(k)
+				text = fd.read()
+			print(len(text))
 			print ("participant %d" %participant)
