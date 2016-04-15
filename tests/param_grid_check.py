1b1c4772bc0bbe005cf1c25354c49ee14de740c9
giffy1 <giffy1991@gmail.com>
2016-04-15 10:33:23 -0400

    Get string of best params, later for parsing it

--- tests/param_grid_check.py
+++ tests/param_grid_check.py
@@ -6,6 +6,8 @@ Created on Fri Apr 15 10:02:08 2016
 """
 
 import os
+search = "Best params:"
+l = len(search)
 for root, dirs, files in os.walk("."):
 	for f in files:
 		if f.endswith(".txt") and f.startswith("log"):
@@ -16,9 +18,13 @@ for root, dirs, files in os.walk("."):
 						participant = int(s[1:])
 			with open(os.path.join(root, f), 'rb') as fd:
 				text = fd.read()
-			i=text.index("Best params:")
-			print(text[i:i+35])
-			print ("participant %d" %participant)
+			try:
+				i=text.index(search)
+				j=text[i:].index("}")
+				print(text[i+l:i+l+j])
+				print ("participant %d" %participant)
+			except:
+				pass
 		elif f.endswith("log.txt") and f.startswith("lopo"):
 			print(os.path.join(root, f))
 			for s in f[f.index("_"):f.index("log.txt")].split("_"):
@@ -27,6 +33,10 @@ for root, dirs, files in os.walk("."):
 						participant = int(s[1:])
 			with open(os.path.join(root, f), 'rb') as fd:
 				text = fd.read()
-			i=text.index("Best params:")
-			print(text[i:i+35])
-			print ("participant %d" %participant)
+			try:
+				i=text.index(search)
+				j=text[i:].index("}")
+				print(text[i+l:i+l+j])
+				print ("participant %d" %participant)
+			except:
+				pass
