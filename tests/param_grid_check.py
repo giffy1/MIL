e83536a35ae315d7e9030a184c8f11ec6ca97d92
giffy1 <giffy1991@gmail.com>
2016-04-15 10:11:21 -0400

    Added check for most frequent optimal params

---
+++ tests/param_grid_check.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 15 10:02:08 2016
+
+@author: snoran
+"""
+
+import os
+import pickle
+for root, dirs, files in os.walk("/mydir"):
+	for f in files:
+		if f.endswith(".pickle") and f.startswith("lopo"):
+			for s in f[f.index("_"):f.index(".pickle")].split("_"):
+				if s[0] == "p":
+					participant = int(s[1:])
+			with open(os.path.join(root, f), 'rb') as fd:
+				r = pickle.load(fd)
+			k = r["Results"].keys()
+			print(k)
+			print ("participant %d" %participant)
