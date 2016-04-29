0dfe3f7e029f9f92b39fc5ce03c2c764ffaa3ec2
giffy1 <giffy1991@gmail.com>
2016-04-29 11:04:43 -0400

    Re-run for M=500, bag sizes 50, 100, 200

--- src/evaluate.py
+++ src/evaluate.py
@@ -23,9 +23,9 @@ from qsub import qsub
 participants = range(1)
 N = 1000 # N : number of instances per participant put into bags
 #M = [0, 12, 25, 50] # M : number of single-instance bags per participant
-bag_sizes = [1, 10, 20, 50, 100, 200]
+bag_sizes = [50, 100, 200] # [1, 10, 20, 50, 100, 200]
 
-M=[0,125,250,500]
+M=[500] #[0,125,250,500]
 
 local = False
 
