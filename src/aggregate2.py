3d168a29d0aba43c11711739bf8c60021a86d8c6
giffy1 <giffy1991@gmail.com>
2016-05-04 11:36:58 -0400

    Can specify participants over which to show plot

--- src/aggregate2.py
+++ src/aggregate2.py
@@ -14,41 +14,30 @@ import numpy as np
 from matplotlib import pyplot as plt
 from util import accuracy_precision_recall_fscore
 
-def main(working_dir):
+participants = [3]
 
-	if not os.path.isdir(working_dir):
-		os.mkdir(working_dir, 0755)
-	
-	log_dir = working_dir + '/log'
-	if not os.path.isdir(log_dir):
-		os.mkdir(log_dir, 0755)
-	
-	err_dir = working_dir + '/err'
-	if not os.path.isdir(err_dir):
-		os.mkdir(err_dir, 0755)
-		
+def main(working_dir):	
 	res_dir = working_dir + '/res'
-	if not os.path.isdir(res_dir):
-		os.mkdir(res_dir, 0755)
-		
+
 	files = [f for f in os.listdir(res_dir) if os.path.isfile(os.path.join(res_dir,f)) and f.startswith("lopo_") and f.endswith(".pickle")]
 	
 	confusion_matrix = {}
 	fscores = {}
 	for f in files:
-		#_p_index = f.index("_p")
+		_p_index = f.index("_p")
 		_b_index = f.index("_b")
 		_m_index = f.index("_n")
 		_i_index = f.index("_i")
-		#p = int(f[_p_index+2:_b_index])
+		p = int(f[_p_index+2:_b_index])
 		b = int(f[_b_index+2:_m_index])
 		n = int(f[_m_index+2:_i_index])
 		
-		with open(os.path.join(res_dir,f), 'rb') as fd:
-			r = pickle.load(fd)
-		conf = r["Results"]["Confusion Matrix"]["Test"]
-		total_conf = confusion_matrix.get((b,n), np.asarray([[0,0],[0,0]]))
-		confusion_matrix[(b,n)] = total_conf + conf
+		if p in participants:
+			with open(os.path.join(res_dir,f), 'rb') as fd:
+				r = pickle.load(fd)
+			conf = r["Results"]["Confusion Matrix"]["Test"]
+			total_conf = confusion_matrix.get((b,n), np.asarray([[0,0],[0,0]]))
+			confusion_matrix[(b,n)] = total_conf + conf
 	for k,conf in confusion_matrix.iteritems():
 		_, _, fscore = accuracy_precision_recall_fscore(conf)[1][1]
 		fscores[k] = fscore
@@ -66,7 +55,7 @@ def main(working_dir):
 	
 if __name__ == "__main__":
 	parser = ArgumentParser()
-	parser.add_argument("-w", "--cwd", dest="working_dir", default='eval12', type=str, help="")
+	parser.add_argument("-w", "--cwd", dest="working_dir", default='eval_lab20_nsessions', type=str, help="")
 	
 	args = parser.parse_args()
 	
