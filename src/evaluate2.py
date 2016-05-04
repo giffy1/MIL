70432a0d61049fab2ae815cc7ea00cf78c6b9db5
giffy1 <giffy1991@gmail.com>
2016-05-04 09:52:19 -0400

    Changed global vars into command line options

--- src/evaluate2.py
+++ src/evaluate2.py
@@ -12,20 +12,21 @@ import sys
 from argparse import ArgumentParser
 from bagging import main as bag_data
 from lopo import main as lopo
+import json
 
 sys.path.insert(0, '../tests')
 from qsub import qsub
 
-participants = range(20)
+#participants = range(20)
 #M = [0, 12, 25, 50] # M : number of single-instance bags per participant
-bag_sizes = [-1] #1,10,20,50,100] #[1, 10, 20, 50, 100]
-
-N = {-1 : range(2,10)}
-#N = {1 : [0,25,50,75,100,125,150,175,200], 10 : range(0,101,10), 20: range(0,51,5), 50: range(0,20,4), 100: range(0,10,2)} # N : number of instances per participant put into bags
-
-M=125
-
-local = True
+#bag_sizes = [-1] #1,10,20,50,100] #[1, 10, 20, 50, 100]
+#
+#N = {-1 : range(2,10)}
+##N = {1 : [0,25,50,75,100,125,150,175,200], 10 : range(0,101,10), 20: range(0,51,5), 50: range(0,20,4), 100: range(0,10,2)} # N : number of instances per participant put into bags
+#
+#M=125
+#
+#local = False
 
 #M = [0, 75, 150, 225, 300]
 #bag_sizes = [1, 5, 10, 20, 40]
@@ -34,7 +35,7 @@ local = True
 #complete all of b=20
 #complete b=50,100 (currently on server)
 
-def main(working_dir, data_dir, n_jobs, n_trials, n_iter):
+def main(working_dir, data_dir, n_jobs, n_trials, n_iter, bag_sizes, M, N, participants, local):
 
 	if not os.path.isdir(working_dir):
 		os.mkdir(working_dir, 0755)
@@ -50,6 +51,13 @@ def main(working_dir, data_dir, n_jobs, n_trials, n_iter):
 	res_dir = working_dir + '/res'
 	if not os.path.isdir(res_dir):
 		os.mkdir(res_dir, 0755)
+	
+	N = json.loads(N)
+	bag_sizes = json.loads(bag_sizes)
+	try:
+		participants = range(int(participants))
+	except:
+		participants = json.loads(participants)
 	
 	for b in bag_sizes:
 		for n in N[b]:
@@ -89,6 +97,11 @@ if __name__ == "__main__":
 	parser.add_argument("--n-jobs", dest="n_jobs", default=1, type=int, help="")
 	parser.add_argument("--n-trials", dest="n_trials", default=3, type=int, help="")
 	parser.add_argument("--n-iter", dest="n_iter", default=10, type=int, help="")	
+	parser.add_argument("-B", "--bag-sizes", dest="bag_sizes", default="[-1]", type=str, help="")
+	parser.add_argument("-M", "--n-single-instances", dest="M", default=125, type=int, help="")
+	parser.add_argument("-N", "--n-bags", dest="N", default="[2,3,4,5,6,7,8,9,10]", type=str, help="")
+	parser.add_argument("-p", "--participants", dest="participants", default="20", type=str, help="")
+	parser.add_argument("-l", "--local", dest="local", default=0, type=int, help="")
 	
 	args = parser.parse_args()
 	
