2165551882e6fdaf3cb2538c2c3d726888b65837
giffy1 <giffy1991@gmail.com>
2016-05-06 13:00:00 -0400

    Added file for aggregating evaluations with varying number of bags from held out participant

---
+++ src/aggregate3.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 09:02:56 2016
+
+@author: snoran
+"""
+
+from __future__ import division
+
+import os
+from argparse import ArgumentParser
+import pickle
+import numpy as np
+from matplotlib import pyplot as plt
+from util import accuracy_precision_recall_fscore
+
+participants = range(20)
+
+def main(working_dir):	
+	res_dir = working_dir + '/res'
+
+	files = [f for f in os.listdir(res_dir) if os.path.isfile(os.path.join(res_dir,f)) and f.startswith("lopo_") and f.endswith(".pickle")]
+	confusion_matrix = {}
+	fscores = {}
+	for f in files:
+		_p_index = f.index("_p")
+		_h_index = f.index("_h")
+		_k_index = f.index("_k")
+		_i_index = f.index("_i")
+		_dot_index = f.index(".")
+		p = int(f[_p_index+2:_h_index])
+		h = int(f[_h_index+2:_k_index])
+		k = int(f[_k_index+2:_i_index])
+		i = int(f[_i_index+2:_dot_index])
+		print p,h,k,i
+		
+		if p in participants:
+			with open(os.path.join(res_dir,f), 'rb') as fd:
+				r = pickle.load(fd)
+			conf = r["Results"]["Confusion Matrix"]["Test"]
+			total_conf = confusion_matrix.get((h,k), np.asarray([[0,0],[0,0]]))
+			confusion_matrix[(h,k)] = total_conf + conf
+	for k,conf in confusion_matrix.iteritems():
+		_, _, fscore = accuracy_precision_recall_fscore(conf)[1][1]
+		fscores[k] = fscore
+		print k, fscore
+	keys = np.asarray(sorted(fscores.keys()))
+	H = set(keys[:,0])
+	for h in H:
+		y=zip(*[(k[1],fscores[k]) for k in sorted(fscores.keys()) if k[0]==h])
+		plt.plot(y[0], y[1], label="bag size " + str(h))
+	
+	plt.xlabel("Number of bags")
+	plt.ylabel("F1 Score")
+	plt.title("Performance varying bag size and number of bags")
+	plt.legend(loc=4)
+	plt.show()
+	
+if __name__ == "__main__":
+	parser = ArgumentParser()
+	parser.add_argument("-w", "--cwd", dest="working_dir", default='eval_risq_nbags_heldout', type=str, help="")
+	
+	args = parser.parse_args()
+	
+	main(**vars(args))
