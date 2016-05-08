5a52534dade38a8f52320a11a283023551e35bc1
giffy1 <giffy1991@gmail.com>
2016-05-08 13:42:36 -0400

    Modified params

--- src/evaluate3.py
+++ src/evaluate3.py
@@ -19,8 +19,15 @@ from qsub import qsub
 
 def main(working_dir, data_dir, n_jobs, n_trials, n_iter, bag_size, M, N, participants, local):
 	
-	held_out_bag_sizes = [-1] #[-1,1,5,10,50,100]
-	K = {-1 : range(0,10)} #, 1 : range(0,101,20), 5 : range(0,41,4), 10 : range(0,21,2), 20: range(0,11,2), 50:range(5), 100:range(3)}
+#	held_out_bag_sizes = [-1] #[-1,1,5,10,50,100]
+#	K = {-1 : range(0,10)} #, 1 : range(0,101,20), 5 : range(0,41,4), 10 : range(0,21,2), 20: range(0,11,2), 50:range(5), 100:range(3)}
+	
+#	held_out_bag_sizes = [5,10,50,100]
+#	K = {1 : range(0,201,20), 5 : range(0,41,4), 10 : range(0,21,2), 20: range(0,11,2), 50:range(5), 100:range(3)}
+
+	held_out_bag_sizes = [-1,1,5,10,50,100]
+	K = {-1 : range(5), 1 : range(20,201,40), 5 : range(4,41,8), 10 : range(0,21,4), 20: range(0,11,2), 50:range(5), 100:range(3)}
+	K_max = 200
 
 	if not os.path.isdir(working_dir):
 		os.mkdir(working_dir, 0755)
@@ -54,7 +61,7 @@ def main(working_dir, data_dir, n_jobs, n_trials, n_iter, bag_size, M, N, partic
 					err_file = os.path.join(err_dir, 'err' + file_str + '.txt')
 					
 					if local:
-						bag_data(data_dir, data_file, bag_size, p, M, N, i, shuffle_bags=True, shuffle_si=True, K=k, K_max = 100, held_out_b=h)
+						bag_data(data_dir, data_file, bag_size, p, M, N, i, shuffle_bags=False, shuffle_si=False, K=k, K_max = K_max, held_out_b=h, shuffle_heldout=True)
 						lopo(data_file, 'sbMIL("verbose":0)', 'randomized', n_iter, n_jobs, 0, save_path, '')
 					else:
 #						#don't submit jobs for bagging, the overhead is too large:
@@ -73,16 +80,16 @@ def main(working_dir, data_dir, n_jobs, n_trials, n_iter, bag_size, M, N, partic
 if __name__ == "__main__":
 	parser = ArgumentParser()
 	parser.add_argument("-d", "--data-dir", dest="data_dir", \
-		default='../data/eating_detection_inertial_ubicomp2015/', type=str, help="")
+		default='../data/smoking-data/', type=str, help="")
 	parser.add_argument("-w", "--cwd", dest="working_dir", \
-		default='eval_lab20_nsessions2', type=str, help="")
+		default='eval_risq_nsessions_heldout1', type=str, help="")
 	parser.add_argument("--n-jobs", dest="n_jobs", default=1, type=int, help="")
-	parser.add_argument("--n-trials", dest="n_trials", default=5, type=int, help="")
-	parser.add_argument("--n-iter", dest="n_iter", default=20, type=int, help="")	
+	parser.add_argument("--n-trials", dest="n_trials", default=1, type=int, help="")
+	parser.add_argument("--n-iter", dest="n_iter", default=1, type=int, help="")	
 	parser.add_argument("-b", "--bag-size", dest="bag_size", default=10, type=int, help="")
-	parser.add_argument("-m", "--n-single-instances", dest="M", default=100, type=int, help="")
+	parser.add_argument("-m", "--n-single-instances", dest="M", default=125, type=int, help="")
 	parser.add_argument("-n", "--n-bags", dest="N", default=10, type=int, help="")
-	parser.add_argument("-p", "--participants", dest="participants", default="[0]", type=str, help="")
+	parser.add_argument("-p", "--participants", dest="participants", default="[3]", type=str, help="")
 	parser.add_argument("-l", "--local", dest="local", default=1, type=int, help="")
 	
 	args = parser.parse_args()
