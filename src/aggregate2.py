a18a76b8cc957d00fd68846d51eb7ac46c0a997f
giffy1 <giffy1991@gmail.com>
2016-05-09 12:40:22 -0400

    .

--- src/aggregate2.py
+++ src/aggregate2.py
@@ -39,6 +39,7 @@ def main(working_dir):
 			confusion_matrix[(b,n)] = total_conf + conf
 	for k,conf in confusion_matrix.iteritems():
 		_, _, fscore = accuracy_precision_recall_fscore(conf)[1][1]
+		print k, fscore
 		if k[0]==0:
 			fscores[k] = .6403
 		else:
